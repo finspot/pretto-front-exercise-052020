6f9ace650e18a244b99a3fa42071b8995dc57156
Camilosorio <tech@camilo.fr>
2020-06-10 16:15:33 +0200

    Exercice done ✔

--- src/Title/index.js
+++ src/Title/index.js
@@ -2,6 +2,6 @@ import React from "react"
 
 import * as S from "./styles.css"
 
-const Title = () => <h1 className={S.title}>Hello friend!</h1>
+const Title = () => <h1 className={S.title}>Growth Engineer Tests - exercice 1 by Camilo</h1>
 
 export default Title
