bbcbc8b14040a042b3b9be98c218195651063043
Camilosorio <tech@camilo.fr>
2020-06-10 15:17:00 +0200

    Render the banner component

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react"
 
 import Title from "../Title"
+import Banner from '../Components/banner'
 
 import "./styles.css"
 
+
 class App extends Component {
   render() {
-    return <Title />
+    return (
+      <>
+        <Title />
+        <Banner />
+      </>
+    );
   }
 }
 
